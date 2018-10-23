529b25df002eb669a86f560f6003c047e728af92
Stephen Goeppele <s.goeppele.parrish@gmail.com>
2018-10-23 14:36:26 -0700

    Update dotfiles

---
+++ setup.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env sh
+
+# This is a setup script for new personal desktop machines running Ubuntu 18.04.
+
+set -eu
+
+# if dual-booting windows, use local time
+timedatectl set-local-rtc 1
+
+# development tools
+sudo apt install git vim neovim stow rxvt-unicode-256color python3-pip
+
+# fun stuff
+sudo apt install anki mpv ffmpeg ffmpeg-thumbnailer neofetch weechat \
+  lftp mediainfo transmission-cli minidlna rofi mpd ncmpcpp torbrowser-launcher
+
+# extra stuff from snapcraft
+sudo snap install brave gimp intellij-idea-{community,ultimate} mcomix-tabetai
+sudo snap connect mcomix-tabetai:removable-media core:removable-media
+
+# fonts
+# - siji
+# - tamzen
+
+# language support
+# - japanese (mozc)
+# - korean
+
+# need to manually install:
+# - google-chrome
+# - ooogle-musicmanager
+# - firefox
+# - calibre
+# - i3-gaps
+# - polybar
+# - go (language)
+# - rustup
+#   - rust (nightly)
+# - sdkman
+#   - java
+#   - scala
+#   - leiningen
+#   - gradle
+#   - maven
+
+# setup ssh server
+# https://dev.to/zduey/how-to-set-up-an-ssh-server-on-a-home-computer
+
+# install noip2 duc
