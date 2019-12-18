3ae3802f39af0cd7350aa63a03e55b9112114f60
Stephen Goeppele <s.goeppele.parrish@gmail.com>
2019-12-17 18:21:28 -0800

    Update lein configuration

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,2 +1,6 @@
 {:user {:plugins [[cider/cider-nrepl "0.15.1"]
-                  [venantius/ultra "0.5.1"]]}}
+                  [venantius/ultra "0.5.1"]]}
+ :repl {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
+                  [refactor-nrepl "2.0.0-SNAPSHOT"]]
+        :dependencies [[alembic "0.3.2"]
+                       [org.clojure/tools.nrepl "0.2.12"]]}}
